b9d55af20bd699070fbf0d225357982c9a1fb547
BuildTools <unconfigured@null.spigotmc.org>
2019-04-23 03:08:42 -0400

    Initial commit

---
+++ Assets/Scripts/PowerUps.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerUps : MonoBehaviour
+{
+    public float multiplier = 1.4f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PickUp(other);
+        }
+    }
+
+    void PickUp(Collider2D player)
+    {
+        PlayerController stats = player.GetComponent<PlayerController>();
+        stats.speed *= multiplier;
+        Destroy(gameObject);
+    }
+}
